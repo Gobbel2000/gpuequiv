80266e2be3857f634ef3a1a7ac4701c289767989
Gabriel Vogel <gabriel.vogel@online.de>
2023-09-14 14:49:16 +0200

    Use CSR sparse matrix encoding for graph in shader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::borrow::Cow;
+use std::iter;
+
 use wgpu::util::DeviceExt;
 
 const WORKGROUP_SIZE: u32 = 64;
@@ -6,23 +8,40 @@ const INF: u32 = 1 << 31;
 
 #[derive(Debug, Clone)]
 struct GameGraph {
-    n_vertices: usize,
-    adj_matrix: Vec<Vec<bool>>,
+    n_vertices: u32,
+    adj: Vec<Vec<u32>>,
     attacker_pos: Vec<bool>,
 }
 
 impl GameGraph {
-    fn new(n_vertices: usize, edges: &[(usize, usize)], attacker_pos: &[bool]) -> Self {
-        let mut adj_matrix = vec![vec![false; n_vertices]; n_vertices];
+    fn new(n_vertices: u32, edges: &[(u32, u32)], attacker_pos: &[bool]) -> Self {
+        let mut adj = vec![vec![]; n_vertices as usize];
         for (from, to) in edges {
-            adj_matrix[*from][*to] = true;
+            adj[*from as usize].push(*to);
         }
 
         Self {
             n_vertices,
-            adj_matrix,
+            adj,
             attacker_pos: attacker_pos.to_vec(),
         }
+    }
+
+    fn csr(&self) -> (Vec<u32>, Vec<u32>) {
+        let column_indices = self.adj.iter()
+            .flatten()
+            .copied()
+            .collect();
+        // Cumulative sum of all list lengths. 0 is prepended manually
+        let row_offsets = iter::once(0).chain(
+            self.adj.iter()
+            .scan(0, |state, adj| {
+                *state = *state + adj.len() as u32;
+                Some(*state)
+            }))
+            .collect();
+        println!("{:?}\n{:?}\n{:?}", self.adj, column_indices, row_offsets);
+        (column_indices, row_offsets)
     }
 }
 
@@ -101,10 +120,10 @@ async fn execute_gpu(graph: &GameGraph) -> Vec<Option<u32>> {
 async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &GameGraph) -> Vec<Option<u32>> {
     // Boolean array to indicate the nodes that should be reached in the reachability game
     // Each bool is saved as u32 so it can be copied to the GPU
-    let to_reach: Vec<u32> = graph.adj_matrix.iter()
+    let to_reach: Vec<u32> = graph.adj.iter()
         .zip(&graph.attacker_pos)
         // 1 if no outgoing edges and defender position, 0 otherwise
-        .map(|(adj, atk)| u32::from(!atk && !adj.iter().any(|e| *e)))
+        .map(|(adj, atk)| u32::from(!atk && adj.is_empty()))
         .collect();
 
     // BUFFERS
@@ -112,14 +131,16 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
     // Copy the matrix into a flat array for the buffer
     // For now, bools are stored as u32, because the WGSL bool type cannot be used in storage
     // buffers (bool is not host-shareable).
-    let flat_matrix: Vec<u32> = graph.adj_matrix
-        .iter()
-        .flatten()
-        .map(|b| u32::from(*b))
-        .collect();
-    let adj_matrix_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("Input graph adjacency matrix storage bufer"),
-        contents: bytemuck::cast_slice(&flat_matrix),
+    let (c, r) = graph.csr();
+    let column_indices_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Input graph column indices storage bufer"),
+        contents: bytemuck::cast_slice(&c),
+        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
+    });
+
+    let row_offsets_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Input graph row offsets storage buffer"),
+        contents: bytemuck::cast_slice(&r),
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
     });
 
@@ -133,7 +154,7 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
     });
 
-    let output_size = (graph.n_vertices * std::mem::size_of::<u32>()) as u64;
+    let output_size = (graph.n_vertices as usize * std::mem::size_of::<u32>()) as u64;
 
     // Indicates whether each node should be visited in this iteration
     let visit_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
@@ -144,7 +165,7 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
             | wgpu::BufferUsages::COPY_DST
     });
 
-    let init_n_steps = vec![INF; graph.n_vertices];
+    let init_n_steps = vec![INF; graph.n_vertices as usize];
     // Output buffer that holds the number of steps for each node required to reach a wanted end point
     let output_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("Shader output buffer"),
@@ -165,28 +186,8 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: Some("Main bind group layout"),
         entries: &[
-            wgpu::BindGroupLayoutEntry {  // adj_matrix
-                binding: 0,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-            wgpu::BindGroupLayoutEntry {  // attacker_pos
-                binding: 1,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: true },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
             wgpu::BindGroupLayoutEntry {  // visit_buffer
-                binding: 2,
+                binding: 0,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Storage { read_only: false },
@@ -196,7 +197,7 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
                 count: None,
             },
             wgpu::BindGroupLayoutEntry {  // output_buffer
-                binding: 3,
+                binding: 1,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Storage { read_only: false },
@@ -214,19 +215,66 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource: adj_matrix_buffer.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 1,
-                resource: attacker_pos_buffer.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 2,
                 resource: visit_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
-                binding: 3,
+                binding: 1,
                 resource: output_buffer.as_entire_binding(),
+            },
+        ],
+    });
+
+    let graph_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label: Some("Input graph bind group layout"),
+        entries: &[
+            wgpu::BindGroupLayoutEntry {  // graph column indices
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {  // graph row offsets
+                binding: 1,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {  // attacker_pos
+                binding: 2,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+        ],
+    });
+
+    let graph_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label: Some("Input graph bind group"),
+        layout: &graph_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: column_indices_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: row_offsets_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: attacker_pos_buffer.as_entire_binding(),
             },
         ],
     });
@@ -240,7 +288,7 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
         label: Some("Pipeline layout"),
-        bind_group_layouts: &[&bind_group_layout],
+        bind_group_layouts: &[&bind_group_layout, &graph_bind_group_layout],
         push_constant_ranges: &[],
     });
 
@@ -261,6 +309,7 @@ async fn execute_gpu_inner(device: &wgpu::Device, queue: &wgpu::Queue, graph: &G
             let mut cpass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor { label: None });
             cpass.set_pipeline(&pipeline);
             cpass.set_bind_group(0, &bind_group, &[]);
+            cpass.set_bind_group(1, &graph_bind_group, &[]);
 
             // Ceil( n / WORKGROUP_SIZE )
             let n = graph.n_vertices as u32;
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,18 +1,26 @@
 @group(0) @binding(0)
-var<storage> adj_matrix: array<u32>;
-
-@group(0) @binding(1)
-var<storage> attacker_pos: array<u32>;
-
-@group(0) @binding(2)
 var<storage, read_write> visit: array<u32>;
 
-@group(0) @binding(3)
+@group(0) @binding(1)
 var<storage, read_write> output: array<u32>;
 
+
+@group(1) @binding(0)
+var<storage> graph_c: array<u32>;
+
+@group(1) @binding(1)
+var<storage> graph_r: array<u32>;
+
+@group(1) @binding(2)
+var<storage> attacker_pos: array<u32>;
+
 fn edge(v_from: u32, to: u32) -> bool {
-    let n = arrayLength(&visit);
-    return adj_matrix[v_from * n + to] != 0u; 
+    for (var j: u32 = graph_r[v_from]; j < graph_r[v_from + 1u]; j++) {
+        if (graph_c[j] == to) {
+            return true;
+        }
+    }
+    return false;
 }
 
 @compute
@@ -30,17 +38,19 @@ fn main(@builtin(global_invocation_id) id: vec3<u32>) {
     if attacker_pos[i] != 0u {
         // Pick minimal step count from next nodes
         new_steps = output[i];
-        for (var v: u32 = 0u; v < n; v++) {
-            if edge(i, v) && output[v] + 1u < new_steps {
-                new_steps = output[v] + 1u;
+        for (var j: u32 = graph_r[i]; j < graph_r[i + 1u]; j++) {
+            let w = graph_c[j];
+            if output[w] + 1u < new_steps {
+                new_steps = output[w] + 1u;
             }
         }
     } else {
         // Pick maximal step count from next nodes
         new_steps = 0u;
-        for (var v: u32 = 0u; v < n; v++) {
-            if edge(i, v) && output[v] >= new_steps {
-                new_steps = output[v] + 1u;
+        for (var j: u32 = graph_r[i]; j < graph_r[i + 1u]; j++) {
+            let w = graph_c[j];
+            if output[w] >= new_steps {
+                new_steps = output[w] + 1u;
             }
         }
     }
