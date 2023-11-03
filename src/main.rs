6d5353874625cdd612de39113ccfd7cbe112f3c9
Gabriel Vogel <gabriel.vogel@online.de>
2023-11-03 21:26:12 +0100

    Make current examples into automated tests

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,5 @@
 use gpuequiv::*;
 
-// Only one-dimensional energies
-fn example_game() -> EnergyGame {
-    //let attacker_pos = vec![true; 18];
-    let attacker_pos: Vec<bool> = (0..18)
-        .map(|i| [0, 2, 4, 6, 9, 11, 12, 14, 17].contains(&i))
-        .collect();
-    let graph = GameGraph::new(
-        18,
-        &[
-            (0, 1, update![-1]),
-            (1, 2, update![-1]),
-            (2, 3, update![-1]),
-            (3, 4, update![-1]),
-            (4, 5, update![-1]),
-            (5, 6, update![-1]),
-            (6, 7, update![-1]),
-            (6, 8, update![-1]),
-            (8, 4, update![-1]),
-            (1, 9, update![-1]),
-            (9, 10, update![-1]),
-            (10, 11, update![-1]),
-            (11, 3, update![-1]),
-            (10, 12, update![-1]),
-            (12, 10, update![-1]),
-            (0, 13, update![-1]),
-            (13, 14, update![-1]),
-            (14, 15, update![-1]),
-            (14, 16, update![-1]),
-            (16, 17, update![-1]),
-        ],
-        &attacker_pos,
-    );
-    EnergyGame::from_graph(graph)
-        .with_reach(vec![7, 15])
-}
-
 // Varied, multidimensional updates, leading to multidimensional energies
 fn multidimensional() -> EnergyGame {
     let attacker_pos: Vec<bool> = (0..18)
@@ -71,23 +35,8 @@ fn multidimensional() -> EnergyGame {
         .with_reach(vec![7, 15])
 }
 
-// Pareto front with multiple energy tuples
-fn multifront() -> EnergyGame {
-    let graph = GameGraph::new(
-        4,
-        &[
-            (0, 1, update![0, 0, 0, -1]),
-            (1, 2, update![-1, 0]),
-            (1, 3, update![0, -1]),
-        ],
-        &[false, true, false, false],
-    );
-    EnergyGame::from_graph(graph)
-        .with_reach(vec![2, 3])
-}
-
 async fn execute() {
-    let mut game = multifront();
+    let mut game = multidimensional();
     let energies = game.run().await.unwrap();
     println!("{:#?}", energies);
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,67 +1,120 @@
 use super::*;
 
-#[test]
-fn reachability_small() {
-    pollster::block_on(execute_reachability_small());
-}
-
-fn example_graph() -> GameGraph {
-    /*
+fn simple_graph() -> EnergyGame {
     let attacker_pos: Vec<bool> = (0..18)
         .map(|i| [0, 2, 4, 6, 9, 11, 12, 14, 17].contains(&i))
         .collect();
-        */
-    let attacker_pos = vec![true; 18];
-    GameGraph::new(
+    let graph = GameGraph::new(
         18,
         &[
-            (0, 1, 0.into()),
-            (1, 2, 0.into()),
-            (2, 3, 0.into()),
-            (3, 4, 0.into()),
-            (4, 5, 0.into()),
-            (5, 6, 0.into()),
-            (6, 7, 0.into()),
-            (6, 8, 0.into()),
-            (8, 5, 0.into()),
-            (1, 9, 0.into()),
-            (9, 10, 0.into()),
-            (10, 11, 0.into()),
-            (11, 3, 0.into()),
-            (10, 12, 0.into()),
-            (12, 10, 0.into()),
-            (0, 13, 0.into()),
-            (13, 14, 0.into()),
-            (14, 15, 0.into()),
-            (14, 16, 0.into()),
-            (16, 17, 0.into()),
+            (0, 1, update![0, 0, -1]),
+            (1, 2, update![0, -1, 0]),
+            (2, 3, update![0, Upd::Min(0)]),
+            (3, 4, update![0, 0, 0, -1]),
+            (4, 5, update![-1, 0]),
+            (5, 6, update![0, -1]),
+            (6, 7, update![-1, 0]),
+            (6, 8, update![0, 0, Upd::Min(0)]),
+            (8, 4, update![0, -1, 0]),
+            (1, 9, update![0, 0, -1]),
+            (9, 10, update![0, 0, -1]),
+            (10, 11, update![0, 0, 0, -1]),
+            (11, 3, update![Upd::Min(2)]),
+            (10, 12, update![0, 0, -1]),
+            (12, 10, update![0, 0, -1]),
+            (0, 13, update![Upd::Min(1)]),
+            (13, 14, update![-1]),
+            (14, 15, update![0, -1]),
+            (14, 16, update![0, 0, -1]),
+            (16, 17, update![0, 0, -1]),
         ],
         &attacker_pos,
-    )
+    );
+    EnergyGame::from_graph(graph)
+        .with_reach(vec![7, 15])
 }
 
-async fn execute_reachability_small() {
-    let mut game = EnergyGame::from_graph(example_graph());
-    game.to_reach = vec![7, 15];
-    let mut runner = game.get_gpu_runner().await;
-    assert_eq!(runner.execute_gpu().await, Ok(vec![
-        Some(3),
-        None,
-        Some(5),
-        Some(4),
-        Some(3),
-        Some(2),
-        Some(1),
-        Some(0),
-        Some(4),
-        None,
-        None,
-        Some(5),
-        None,
-        Some(2),
-        Some(1),
-        Some(0),
-        None,
-        None,
-    ]));
+#[pollster::test]
+async fn simple() {
+    let mut game = simple_graph();
+    assert_eq!(game.run().await.unwrap(),
+        &[
+           vec![energy![1, 1]], // 0
+           vec![], // 1
+           vec![energy![2, 1, 0, 1]], // 2
+           vec![energy![2, 1, 0, 1]], // 3
+           vec![energy![2, 1]], // 4
+           vec![energy![1, 1]], // 5
+           vec![energy![1]], // 6
+           vec![energy![0]], // 7
+           vec![energy![2, 2]], // 8
+           vec![], // 9
+           vec![], // 10
+           vec![energy![2, 1, 2, 1]], // 11
+           vec![], // 12
+           vec![energy![1, 1]], // 13
+           vec![energy![0, 1]], // 14
+           vec![energy![0]], // 15
+           vec![], // 16
+           vec![], // 17
+        ]
+    );
+}
+
+#[pollster::test]
+async fn simple_one_dimension() {
+    let mut game = simple_graph();
+    // Set all updates (edge weights) to -1, 0, 0, ...
+    for node in game.graph.weights.iter_mut() {
+        for w in node.iter_mut() {
+            *w = update![-1];
+        }
+    }
+    // Energies saturate at 3
+    assert_eq!(game.run().await.unwrap(),
+        &[
+            vec![energy![3]],
+            vec![],
+            vec![energy![3]],
+            vec![energy![3]],
+            vec![energy![3]],
+            vec![energy![2]],
+            vec![energy![1]],
+            vec![energy![0]],
+            vec![energy![3]],
+            vec![],
+            vec![],
+            vec![energy![3]],
+            vec![],
+            vec![energy![2]],
+            vec![energy![1]],
+            vec![energy![0]],
+            vec![],
+            vec![],
+        ]
+    );
+}
+
+// Test a small example where a multi-energy pareto front is created
+#[pollster::test]
+async fn fork() {
+    let graph = GameGraph::new(
+        4,
+        &[
+            (0, 1, update![0, 0, 0, -1]),
+            (1, 2, update![-1, 0]),
+            (1, 3, update![0, -1]),
+        ],
+        &[false, true, false, false],
+    );
+    let mut game = EnergyGame::from_graph(graph)
+        .with_reach(vec![2, 3]);
+    assert_eq!(game.run().await.unwrap(),
+        &[
+            vec![energy![1, 0, 0, 1], energy![0, 1, 0, 1]],
+            vec![energy![1], energy![0, 1]],
+            vec![energy![0]],
+            vec![energy![0]],
+        ]
+    );
 }
