6dda8f53a9424682850cb06c3138ea962f977c24
Gabriel Vogel <gabriel.vogel@online.de>
2024-01-19 22:44:37 +0100

    Update wgpu to 0.19

--- src/energygame/gpuutils.rs
+++ src/energygame/gpuutils.rs
@@ -33,8 +33,8 @@ impl GPUCommon {
         Ok(adapter.request_device(
                 &wgpu::DeviceDescriptor {
                     label: None,
-                    features: wgpu::Features::empty(),
-                    limits: wgpu::Limits::downlevel_defaults(),
+                    required_features: wgpu::Features::empty(),
+                    required_limits: wgpu::Limits::downlevel_defaults(),
                 },
                 None,
             )
